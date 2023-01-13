a7b56dd5c8fe05625de80a8ece30e876841d9ca2
Beixa <beitia.iker@gmail.com>
2023-01-13 19:21:49 +0100

    Questions to external const
     Please enter the commit message for your changes. Lines starting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import {Questions} from './questions/questions.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const handleAnswerButtonClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score+1)
+    }
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < Questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(!showScore);
+    }    
+  }; 
+
+	return (
+    <div className="app">
+      {showScore ? (
+        <div className="score-section">
+          You scored {score} out of {Questions.length}
+        </div>
+      ) : (
+        <>
+          <div className="question-section">
+            <div className="question-count">
+              <span>Question {currentQuestion+1}</span>/{Questions.length}
+            </div>
+            <div className="question-text">{Questions[currentQuestion].questionText}</div>
+          </div>
+          <div className="answer-section">
+            {Questions[currentQuestion].answerOptions.map((answerOption, index) => (
+              <button onClick = { () => handleAnswerButtonClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
-
-export default App;
